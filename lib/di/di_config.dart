0f4a0d5e92447b835caea8c3e7197746cbbaab81
broisnischal <neeswebservice@gmail.com>
2025-02-25 00:37:01 +0545

    chore:init-project

--- lib/di/di_config.dart
+++ lib/di/di_config.dart
@@ -1,29 +1,4 @@
-// // di.config.dart
-// import 'package:injectable/injectable.dart'
-//     hide Environment; // Hide injectable's Environment
-// import 'package:loop/core/sys/environment.dart'; // Your custom Environment
-// import 'package:loop/core/sys/flavor.dart';
-
-// @module
-// abstract class AppModule {
-//   @FlavorConfig(
-//     Flavor.development,
-//   ) // Now clearly refers to your custom annotation
-//   @singleton
-//   Environment get devEnv =>
-//       DevEnvironment(); // Clearly refers to your Environment cla ss
-
-//   // @FlavorConfig(Flavor.staging)
-//   // @singleton
-//   // Environment get stagingEnv => StagingEnvironment();
-
-//   // @FlavorConfig(Flavor.production)
-//   // @singleton
-//   // Environment get prodEnv => ProdEnvironment();
-// }
-
 // di.config.dart
-// REMOVE THIS ENTIRE BLOCK
 import 'package:injectable/injectable.dart';
 
 @module
